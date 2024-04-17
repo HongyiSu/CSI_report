b385c600182d72cd0dd9bf9a4102d7f4d313fd66
Mike <56286435+HongyiSu@users.noreply.github.com>
2024-04-17 12:04:44 +0200

    MATLAB code for figure 12

---
+++ driver_VR.m
@@ -0,0 +1,48 @@
+% Author: Hongyi Su (hongyi.su@irsn.fr)
+% Last update: 17/4/2024
+% Description: plot a figure which shows the location of virtual nodes
+% Usage: matlab driver_VR.m
+
+clear; clc; close all;
+
+file = 'lat_long_revisee_cut.xlsx';
+[LAT LON dist2epi STA_4 xx yy] = read_mydata(file);
+fault_length = 6; %km
+fault_width = 2.5; %km
+strike = 47;
+dip = 58;
+hypo_depth = 1.0; %km
+[xx_s yy_s]= rotate_sta(xx, yy, strike);
+
+plot_figure3(xx, yy, xx_s, yy_s, STA_4, dist2epi, fault_length);
+
+function [LAT LON dist2epi STA_4 xx yy] = read_mydata(file)
+    % Read the Excel file into a table
+    data2 = readtable(file);
+    sncf_lat = data2.Latitude;
+    sncf_lon = data2.Longitude;
+    sncf_sta = string(data2.Station);
+    Lat = [44.521, 44.374, 44.636, 44.307, 44.356];
+    Lon = [4.669, 4.770, 4.759, 4.689, 4.857];
+    %apprend 14 SNCF stations
+    LAT = [Lat sncf_lat' 44.5263]; 
+    LON = [Lon sncf_lon' 4.6661];
+    [x,y,utmzone] = deg2utm(LAT,LON); % call function deg2utm to convert lat/lon to XY
+    xx = x - x(1);
+    yy = y - y(1);
+    dist2epi = sqrt(xx.^2 + yy.^2);
+    sta = ["Epicenter"; "ADHE";"CRU1";"OGLP";"TRI2"];
+    sta_name = [sta; sncf_sta; "CLAU"];
+    STA_4 = [xx, yy, sta_name, dist2epi];
+end
+
+function [xx_s yy_s] = rotate_sta(xx, yy, strike)
+    strike_radians = deg2rad(strike);
+    R = [cos(strike_radians) sin(strike_radians);
+        -sin(strike_radians) cos(strike_radians)];
+    coordinates = [xx'; yy'];
+    rotated_coordinates = R * coordinates;
+    xx_s = rotated_coordinates(1, :)';
+    yy_s = rotated_coordinates(2, :)';
+end
+
---
+++ plot_figure3.m
@@ -0,0 +1,63 @@
+function plot_figure3(xx, yy, xx_s, yy_s, STA_4, dist2epi, fault_length)
+
+    % Create a figure and define the size
+    X_max = 20;
+    Y_max = 30;
+    figWidth = X_max; % Width in cm
+    figHeight = Y_max; % Height in cm
+    figure('Units', 'centimeters', 'Position', [0, 0, figWidth, figHeight]);
+
+    % Add Fault
+    fault_surface_p1 = [-fault_length/2, 0];
+    fault_surface_p2 = [fault_length/2, 0];
+    plot([fault_surface_p1(1), fault_surface_p2(1)],[fault_surface_p1(2), fault_surface_p2(2)],'r', 'LineWidth', 2);   
+    hold on
+    text((fault_length/2+1), (fault_surface_p2(2)),'LRf','FontSize', 15, 'FontWeight','bold');
+    
+    % Add epicenter, which is the origin (0,0)
+    scatter(xx(1)/1000,yy(1)/1000, 150 ,'p', 'filled','MarkerFaceColor','y','MarkerEdgeColor','k');
+    hold on
+
+    % Add stations after rotation
+    for(i=2:length(xx)-1)
+        scatter(xx_s(i)/1000,yy_s(i)/1000, 100, 'v', 'MarkerFaceColor','r','MarkerEdgeColor', 'b');
+        text((xx_s(i))/1000-2,yy_s(i)/1000+1, strcat(STA_4(i,3), ' (', num2str(round(dist2epi(i)/1000,4,'significant')),'km' ,')'),"FontSize",10,"FontWeight","normal");
+        hold on
+    end
+    % ADD CLAU station
+    scatter(xx_s(end)/1000,yy_s(end)/1000, 100, 'v', 'MarkerFaceColor', 'r','MarkerEdgeColor', 'b');
+    hold on
+    text((xx_s(end))/1000-3,yy_s(end)/1000+2, strcat(STA_4(length(xx),3), ' (', num2str(round(dist2epi(length(xx))/1000,2,'significant')),'km' ,')'),"FontSize",20,"FontWeight","normal");
+
+    % Add a box boudary
+    x_limits = [-10, 10];
+    y_limits = [-10, 10];
+    x_corners = [x_limits(1), x_limits(2), x_limits(2), x_limits(1), x_limits(1)];
+    y_corners = [y_limits(1), y_limits(1), y_limits(2), y_limits(2), y_limits(1)];
+    plot(x_corners, y_corners, 'k-', 'LineWidth', 2);
+
+    %Add label and title
+    xlim([-X_max, X_max]);
+    ylim([-Y_max, Y_max]);
+    xlabel('Easting [km]');
+    ylabel('Northing [km]');
+    title('Location of the Virtual Nodes');
+  
+    set(gca, 'FontSize', 20, 'FontWeight','bold');
+    ax = gca;
+    % Calculate the desired tick length based on the data range
+    x_range = X_max*2; %km
+    y_range = Y_max*2; %km
+    desired_tick_length = 0.0;  % Set your desired tick length here
+    % Calculate the tick length for x and y axes
+    x_tick_length = desired_tick_length * diff(ax.XLim) / x_range;
+    y_tick_length = desired_tick_length * diff(ax.YLim) / y_range;
+    % Set the tick lengths for x and y axes
+    ax.XRuler.TickLength = [x_tick_length, x_tick_length]; % X-axis tick length
+    ax.YRuler.TickLength = [y_tick_length, y_tick_length]; % Y-axis tick length
+    % Enable minor grid
+    grid on;
+    box on;
+    % Add a legend
+    legend('Fault','Epicentre','','Virtual Nodes','Location','northwest');
+end
